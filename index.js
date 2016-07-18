3e1c13473dbe1169a88c465d653890cf1f2dc776
Ruben Vereecken <rubenvereecken@gmail.com>
2016-07-18 11:14:17 +0200

    Changed extension

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var fs = require('fs');
 var protobuf = require('protobuf');
 
-module.exports = new protobuf(fs.readFileSync('compiled.proto'));
+module.exports = new protobuf(fs.readFileSync('compiled.desc'));
